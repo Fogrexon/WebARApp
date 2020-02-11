8fc9e39c5a3f69587175eb4328ce2d875fec2ca6
fogrexon <katsu15873@gmail.com>
2020-02-12 00:11:21 +0900

    test

--- js/WebAR.js
+++ js/WebAR.js
@@ -1,7 +1,9 @@
 import * as THREE from "../jsm/three.module.js";
+import {TubePainter} from '../jsm/TubePainter.js';
+
 import {ARButton} from "../jsm/ARButton.js";
 
-
+/*
 let width = window.innerWidth;
 let height = window.innerHeight;
 
@@ -29,7 +31,7 @@ scene.add(light);
 
 const onSelectStart = ()=>{
     this.userData.isSelecting = true;
-    alert("touched");
+    
 }
 
 const controller = renderer.xr.getController(0);
@@ -73,3 +75,126 @@ const tick = ()=>{
 }
 
 tick();
+
+*/
+			var container;
+			var camera, scene, renderer;
+			var controller, painter;
+
+			var cursor = new THREE.Vector3();
+
+			init();
+			animate();
+
+			function init() {
+
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				scene = new THREE.Scene();
+
+				camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 20 );
+
+				//
+
+				renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				renderer.xr.enabled = true;
+				container.appendChild( renderer.domElement );
+
+				//
+
+				document.body.appendChild( ARButton.createButton( renderer ) );
+
+				// model
+
+				var light = new THREE.HemisphereLight( 0xffffff, 0xbbbbff, 1 );
+				light.position.set( 0, 1, 0 );
+				scene.add( light );
+
+				//
+
+				painter = new TubePainter();
+				painter.setSize( 0.4 );
+				painter.mesh.material.side = THREE.DoubleSide;
+				scene.add( painter.mesh );
+
+				//
+
+				function onSelectStart() {
+
+					this.userData.isSelecting = true;
+					this.userData.skipFrames = 2;
+
+				}
+
+				function onSelectEnd() {
+
+					this.userData.isSelecting = false;
+
+				}
+
+				controller = renderer.xr.getController( 0 );
+				controller.addEventListener( 'selectstart', onSelectStart );
+				controller.addEventListener( 'selectend', onSelectEnd );
+				controller.userData.skipFrames = 0;
+				scene.add( controller );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+			}
+
+			function onWindowResize() {
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			//
+
+			function handleController( controller ) {
+
+				var userData = controller.userData;
+
+				cursor.set( 0, 0, - 0.2 ).applyMatrix4( controller.matrixWorld );
+
+				if ( userData.isSelecting === true ) {
+
+					if ( userData.skipFrames >= 0 ) {
+
+						// TODO(mrdoob) Revisit this
+
+						userData.skipFrames --;
+
+						painter.moveTo( cursor );
+
+					} else {
+
+						painter.lineTo( cursor );
+						painter.update();
+
+					}
+
+				}
+
+			}
+
+			function animate() {
+
+				renderer.setAnimationLoop( render );
+
+			}
+
+			function render() {
+
+				handleController( controller );
+
+				renderer.render( scene, camera );
+
+			}
