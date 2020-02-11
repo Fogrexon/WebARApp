24085642bccd8335ebdd10bb98017b9bd0419d86
fogrexon <katsu15873@gmail.com>
2020-02-11 20:24:26 +0900

    Galaxy.html

---
+++ js/Galaxy.js
@@ -0,0 +1,87 @@
+
+import {vertexSource, fragmentSource} from "../jsm/galaxyShader.js";
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+const renderer = new THREE.WebGLRenderer(
+    {
+        canvas: document.getElementById("cnv")
+    }
+);
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
+camera.position.set(0, 0, 10);
+
+
+const OnResize = () => {
+    width = window.innerWidth;
+    height = window.innerHeight;
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
+}
+
+OnResize();
+window.addEventListener("resize", OnResize);
+
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+controls.enableDamping = true;
+controls.dampingFactor = 0.1;
+
+
+
+const texture = new THREE.TextureLoader().load("./img/cloud.png");
+
+
+let x,y,z,r,rot;
+let geometry,material,cube;
+let uniforms = [];
+
+for(let i=0;i<500;i++){
+    x = (Math.random()-0.5) * 1000;
+    y = (Math.random()-0.5) * 1000;
+    z = (Math.random()-0.5) * 1000;
+    r = (Math.random()) * 20 + 1.0;
+
+    geometry = new THREE.SphereGeometry(r,32,32);
+
+    uniforms.push({
+        time: { type: "f", value: 1.0 },
+        uTex: {value: texture},
+        uCenter: {value: new THREE.Vector3(x,y,z)},
+        uSize: {value: r},
+        uRot: {value: new THREE.Vector3(Math.random()*Math.PI*2,Math.random()*Math.PI*2,Math.random()*Math.PI*2)}
+    });
+
+    material = new THREE.ShaderMaterial({
+        uniforms: uniforms[i],
+        vertexShader: vertexSource,
+        fragmentShader: fragmentSource
+    });
+    material.transparent = true;
+    
+    cube = new THREE.Mesh(geometry, material);
+    cube.position.set(x,y,z);
+    scene.add(cube);
+}
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF);
+directionalLight.position.set(1,1,1);
+scene.add(directionalLight);
+
+const start = Date.now();
+
+const tick = () =>{
+    controls.update();
+    for(let i=0;i<uniforms.length;i++) uniforms[i].time.value = Date.now() - start;
+    renderer.render(scene, camera);
+    setTimeout(tick, 1000 / 60);
+}
+tick();
---
+++ jsm/galaxyShader.js
@@ -0,0 +1,110 @@
+const vertexSource = `
+varying vec2 vUv;
+
+varying vec4 vPosition;
+varying vec3 vCamera;
+
+void main()
+{
+    vPosition = modelMatrix * vec4( position, 1.0 );
+    vCamera = cameraPosition;
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+}
+`;
+
+const fragmentSource = `
+varying vec2 vUv;
+varying vec4 vPosition;
+varying vec3 vCamera;
+
+uniform float time;
+uniform sampler2D uTex;
+uniform vec3 uCenter;
+uniform float uSize;
+uniform vec3 uRot;
+
+vec3 LIGHT_DIR = normalize(vec3(0.74,-0.74,0));
+
+mat4 rotate(vec3 rot)
+{
+    mat4 rx = mat4(
+        1.0,    0.0,        0.0,            0.0,
+        0.0,    cos(-rot.x), sin(-rot.x),    0.0,
+        0.0,    -sin(-rot.x), cos(-rot.x),     0.0,
+        0.0,    0.0,        0.0,            1.0
+    );
+    mat4 ry = mat4(
+        cos(-rot.y),     0.0,    -sin(-rot.y), 0.0,
+        0.0,            1.0,    0.0,        0.0,
+        sin(-rot.y),    0.0,    cos(-rot.y), 0.0,
+        0.0,            0.0,    0.0,        1.0
+    );
+    mat4 rz = mat4(
+        cos(-rot.z), sin(-rot.z),    0.0,    0.0,
+        -sin(-rot.z), cos(-rot.z),     0.0,    0.0,
+        0.0,        0.0,            1.0,    0.0,
+        0.0,        0.0,            0.0,    1.0
+    );
+
+    return (ry * rx * rz);
+}
+
+#define ITERATIONS 8
+
+
+
+
+float map(vec3 pos)
+{
+    pos = pos;
+    float rot = time * 0.001 + pow(length(pos.xz),0.5)*5.0;
+    pos.xz = mat2(cos(rot),-sin(rot),sin(rot),cos(rot)) * pos.xz;
+    float a = max(0.0, texture2D(uTex, (pos.xz+vec2(1.0))*0.5).r - 0.1);
+    return abs(pos.y) - ((a * 0.1) * (1.0 - length(pos.xz)) * length(pos.xz) + (max(0.0,1.0 - length(pos.xz))+0.001)*0.2);
+}
+
+vec3 calcNorm(vec3 pos)
+{
+    float d = 0.0001;
+    float center = map(pos);
+    vec3 grad = vec3(map(pos+vec3(d,0.0,0.0))-center, map(pos+vec3(0.0,d,0.0))-center, map(pos+vec3(0.0,0.0,d))-center);
+    return normalize(grad / d);
+}
+
+// lambert model
+vec4 lambert(vec3 pos)
+{
+    vec3 light = LIGHT_DIR;
+    vec3 n = calcNorm(pos);
+    if(n.y < 0.0) light = - LIGHT_DIR;
+    float b = max(0.0, dot(n, -light))*0.9+0.1;
+    return vec4(b, b, b, 1.0) * mix(vec4(1.5), vec4(0.1,0.2,0.6,0.0), length(pos.xz));
+}
+
+void main() {
+    vec3 dir = normalize(vPosition.xyz - vCamera);
+    vec3 pos = (vPosition.xyz - uCenter)/uSize;
+    mat4 R = rotate(uRot);
+    dir = (R * vec4(dir, 1.0)).xyz;
+    pos = (R * vec4(pos, 1.0)).xyz;
+
+    float d = 0.0;
+
+    for(int i=0;i<1000;i++)
+    {
+        d = map(pos);
+        if(abs(d) < 0.001)
+        {
+            gl_FragColor = lambert(pos);
+            return;
+        }
+        if(length(pos)>=1.0) break;
+        pos += dir * d * 0.2;
+    }
+
+    gl_FragColor=vec4(0.2,0.2,0.0,0.0);
+}
+`;
+
+export {vertexSource, fragmentSource}
