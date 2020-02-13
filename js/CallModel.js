6a006907935611eea5f813562f671dd1837b3c72
fogrexon <katsu15873@gmail.com>
2020-02-14 03:00:37 +0900

    refactoring

--- js/CallModel.js
+++ js/CallModel.js
@@ -2,88 +2,62 @@ import * as THREE from "../jsm/three.module.js";
 import {ARButton} from "../jsm/ARButton.js";
 import {OBJLoader} from "../jsm/OBJLoader.js";
 
-var container;
-var camera, scene, renderer;
-var controller;
+const container = document.createElement("div");
+document.body.appendChild(container);
 
-var reticle;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 2000);
+const light = new THREE.HemisphereLight(0xffffff, 0x5555aa, 1.0);
+light.position.set(0.5, 1.0, 0.25);
+scene.add(light);
 
-var hitTestSource;
-var hitTestSourceRequested = false;
+const renderer = new THREE.WebGLRenderer(
+    {
+        antialiase: true,
+        alpha: true
+    }
+);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.xr.enabled = true;
+container.appendChild(renderer.domElement);
 
-init();
-animate();
 
-function init() {
+document.body.appendChild(ARButton.createButton(renderer, {requiredFeatures: ["hit-test"]}));
 
-    container = document.createElement( 'div' );
-    document.body.appendChild( container );
+let carModel;
+new OBJLoader().load("./models/car.obj", (object)=>{
+    carModel = object.clone();
+    carModel.position.set(0,0,0);
+    carModel.rotation.set(0,0,0);
+    carModel.scale.set(0.03,0.03,0.03);
+});
 
-    scene = new THREE.Scene();
+const reticle = new THREE.Mesh(
+    new THREE.RingBufferGeometry(0.15, 0.2, 32).rotateX(-Math.PI/2),
+    new THREE.MeshBasicMaterial()
+);
+reticle.matrixAutoUpdate = false;
+reticle.visible = false;
+scene.add(reticle);
 
-    camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 2000 );
+const onSelect = () => {
 
-    var light = new THREE.HemisphereLight( 0xffffff, 0x5555aa, 1 );
-    light.position.set( 0.5, 1, 0.25 );
-    scene.add( light );
-
-    //
-
-    renderer = new THREE.WebGLRenderer( { antialias: true, alpha: true } );
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.xr.enabled = true;
-    container.appendChild( renderer.domElement );
-
-    //
-
-    document.body.appendChild( ARButton.createButton( renderer, { requiredFeatures: [ 'hit-test' ] } ) );
-    //
-
-    //var geometry = new THREE.CylinderBufferGeometry( 0.1, 0.1, 0.2, 32 ).translate( 0, 0.1, 0 );
-    let carModel;
-    var ObjLoader = new OBJLoader(); 
-    ObjLoader.load("./models/car.obj",  function (object){
-        carModel = object.clone();
-        carModel.scale.set(0.03 , 0.03, 0.03);            // 縮尺の初期化
-        carModel.rotation.set(0, 0, 0);         // 角度の初期化
-        carModel.position.set(0, 0, 0);         // 位置の初期化                // sceneに追加
-    });
-
-    function onSelect() {
-
-        if ( reticle.visible && !!carModel) {
-
-            // var material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-            var mesh = carModel.clone();
-            mesh.position.setFromMatrixPosition( reticle.matrix );
-            //mesh.position.set(reticle.position.x,reticle.position.y,reticle.position.z);
-            // mesh.scale.y = Math.random() * 2 + 1;
-            scene.add( mesh );
-
-        }
-
+    if ( reticle.visible && !!carModel) {
+        var mesh = carModel.clone();
+        mesh.position.setFromMatrixPosition( reticle.matrix );
+        scene.add( mesh );
     }
 
-    controller = renderer.xr.getController( 0 );
-    controller.addEventListener( 'select', onSelect );
-    scene.add( controller );
+};
 
-    reticle = new THREE.Mesh(
-        new THREE.RingBufferGeometry( 0.15, 0.2, 32 ).rotateX( - Math.PI / 2 ),
-        new THREE.MeshBasicMaterial()
-    );
-    reticle.matrixAutoUpdate = false;
-    reticle.visible = false;
-    scene.add( reticle );
+controller = renderer.xr.getController( 0 );
+controller.addEventListener( 'select', onSelect );
+scene.add( controller );
 
-    //
+//
 
-    window.addEventListener( 'resize', onWindowResize, false );
-
-}
-
-function onWindowResize() {
+const onWindowResize = ()=>{
 
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
@@ -92,26 +66,31 @@ function onWindowResize() {
 
 }
 
-//
+window.addEventListener( 'resize', onWindowResize, false );
 
-function animate() {
 
-    renderer.setAnimationLoop( render );
+const render = () => {
 
 }
 
-function render( timestamp, frame ) {
+
+
+
+
+let hitTestSourceRequested = false
+
+const render = ( timestamp, frame ) => {
 
     if ( frame ) {
 
-        var referenceSpace = renderer.xr.getReferenceSpace();
-        var session = renderer.xr.getSession();
+        const referenceSpace = renderer.xr.getReferenceSpace();
+        const session = renderer.xr.getSession();
 
         if ( hitTestSourceRequested === false ) {
 
-            session.requestReferenceSpace( 'viewer' ).then( function ( referenceSpace ) {
+            session.requestReferenceSpace( 'viewer' ).then( ( referenceSpace ) => {
 
-                session.requestHitTestSource( { space: referenceSpace } ).then( function ( source ) {
+                session.requestHitTestSource( { space: referenceSpace } ).then( ( source ) => {
 
                     hitTestSource = source;
 
@@ -147,3 +126,5 @@ function render( timestamp, frame ) {
     renderer.render( scene, camera );
 
 }
+
+renderer.setAnimationLoop(render)
